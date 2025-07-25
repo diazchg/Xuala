5cf423c4e159118b7526c247b1da3b237b03f456
Gustavo Diaz <diazchg@gmail.com>
2025-07-24 23:18:46 -0300

    added initial scafolding

---
+++ src/main/java/com/uala/xuala/XualaApplication.java
@@ -0,0 +1,13 @@
+package com.uala.xuala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XualaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XualaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uala/xuala/XualaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uala.xuala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XualaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
